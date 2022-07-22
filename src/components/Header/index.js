b5a4247c473a99682e2bbd5481ece56b0d81666f
tmenture <thomasoxemail@gmail.com>
2022-07-21 20:39:00 -0400

    created header index.js file, about index.js file, and contact index.js file

---
+++ src/components/Header/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Navigation from '../Navigation';
+import About from '../About';
+import Contact from '../Contact';
+import Portfolio from '../Portfolio';
+
+function Header () {
+    const [currentPage, handlePageChange] = useState('About');
+
+    const pageLoad = () => {
+        switch (currentPage) {
+            case "About":
+                return <About />;
+            case "Portfolio":
+                return <Portfolio />;
+            case "Contact":
+                return <Contact />;
+
+            default:
+                return <About />;
+            
+        }
+    };
+
+    return (
+        <div>
+
+            <nav className='navbar'>
+                <div className='navbar-brand'>
+                    <a 
+                        className='navbar-item'
+                        rel='noreferrer'
+                        target="_blank"
+                        href='https://github.com/tmenture?tab=repositories'
+                    >
+                        <span className='content is-large'>Thomas Menture</span>
+                    </a>
+                </div>
+            </nav>
+
+            <Navigation
+                currentPage={currentPage}
+                handlePageChange={handlePageChange}
+            />
+
+            <main>
+                <div>{pageLoad(currentPage)}</div>
+            </main>
+            
+        </div>
+    );
+}
+
+export default Header;
